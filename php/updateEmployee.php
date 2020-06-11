228e53ccb1668946a71f1c0034ac25dc62c99bf7
CoreyDennison <cmgdennison@gmail.com>
2020-06-11 16:12:25 +0100

    Coded UPDATE for employee table

--- php/updateEmployee.php
+++ php/updateEmployee.php
@@ -6,7 +6,7 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="../css/insertIntoemployee.css">
+        <link rel="stylesheet" type="text/css" href="../css/updateEmployee.css">
         <link rel="stylesheet" type="text/css" href="../css/box.css">
         <link rel="stylesheet" type="text/css" href="../css/reset.css">
 
@@ -141,48 +141,39 @@
 
         <h3>Add Employee</h3>
         <p id="r_arrow"><a href="insertIntoclients.php">Add client <i style="color: red; ">&xrarr;</i></a></p>
+<?php
 
+$user = 'root';
+$pass = '';
+$db = 'car_dealership';
 
-    <?php
+$db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect!");
 
-        $user = 'root';
-        $pass = '';
-        $db = 'car_dealership';
+$sql = "SELECT * FROM employee";
 
-        $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect!");
+$result = mysqli_query($db, $sql) or die("Unable to gather info.");
 
-        $sql = "SELECT * FROM employee";
+echo "<table>";
+echo "<tr><th>emp_id</th><th>fname</th><th>lname</th><th>DOB</th><th>mgr_id</th><th>salary</th></tr>";
+while($row = mysqli_fetch_assoc($result)){
+    echo "<tr><td>{$row['emp_id']}</td><td>{$row['fname']}</td><td>{$row['lname']}</td><td>{$row['dob']}</td><td>{$row['mng_id']}</td><td>{$row['salary']}</td></tr>";
+}
+echo "</table>";
 
-        $result = mysqli_query($db, $sql) or die("Unable to gather info.");
+$db -> close();
+?>
 
-        echo "<table>";
-        echo "<tr><th>emp_id</th><th>fname</th><th>lname</th><th>DOB</th><th>mgr_id</th><th>salary</th></tr>";
-        while($row = mysqli_fetch_assoc($result)){
-            echo "<tr><td>{$row['emp_id']}</td><td>{$row['fname']}</td><td>{$row['lname']}</td><td>{$row['dob']}</td><td>{$row['mng_id']}</td><td>{$row['salary']}</td></tr>";
-        }
-        echo "</table>";
-
-        $db -> close();
-    ?>
-
-    <br>
-    <form action="insertIntoemployee.php" method="post" value=""><br>
-        <label>Employee ID: </label><input class="num" type="number" name="employee_id" value="" style="width: 50px;"/><br>
-        <label>First Name: </label><input type="text" name="first_name" value=""/><br>
-        <label>Last Name: </label><input type="text" name="last_name" value=""/><br>
-        <label>Date Of Birth: </label><input type="date" name="dob" value=""/><br>
+<form action="updateEmployee.php" method="post" value=""><br>
+        <p>Enter ID of employees whose data you wish to edit: </p><input class="num" type="number" name="employee_id" value="" style="width: 50px;"/><br>
+        <p>Enter new data (If value is unchanged, please leave as current value (e.g if Manager ID is listed as 1, enter Manager ID as 1 below)):</p><br>
         <label>Manager ID: </label><input type="number" name="manager_id" value="" style="width: 50px;"/><br>
         <label>Salary: </label><input type="number" name="salary" value=""/><br>
         <button type="submit" name="submit">Submit</button><br>
     </form>
 
-
     <?php
         if(isset($_POST['submit'])){
             $empID = $_POST["employee_id"];
-            $firstName = $_POST["first_name"];
-            $lastName = $_POST["last_name"];
-            $dob = $_POST["dob"];
             $mgrID = $_POST["manager_id"];
             $salary = $_POST["salary"];
 
@@ -192,7 +183,7 @@
 
             $db = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect!");
 
-            $sql = "INSERT INTO employee (emp_id, fname, lname, dob, mng_id, salary) VALUES($empID, '$firstName', '$lastName', '$dob', $mgrID, $salary);";
+            $sql = "UPDATE `employee` SET `mng_id`= $mgrID,`salary`= $salary WHERE `emp_id` = $empID ";
             mysqli_query($db, $sql);
             
             $db -> close();
@@ -201,7 +192,5 @@
             echo "<meta http-equiv='refresh' content='1'>";
         }   
     ?>
-    
-    </body>
+</body>
 </html>
-
